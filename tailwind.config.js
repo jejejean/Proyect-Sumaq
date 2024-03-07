e1f64031e5d0c1c803b767c53d51a4901a474683
jejejean <u19212532@utp.edu.pe>
2024-03-07 14:33:10 -0500

    style: :rocket: Complete header implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        // sm: { min: "0px", max: "570px" },
-        // md: { min: "571px", max: "900px" },
-        // lg: { min: "901px", max: "1024px" },
-        // xl: { min: "1025px" },
+        sm: { min: "0px", max: "570px" },
+        md: { min: "571px", max: "900px" },
+        lg: { min: "901px", max: "1100px" },
+        custom: { min: "1101px", max: "1300px"},
+        xl: { min: "1301px" },
       },
     },
   },
