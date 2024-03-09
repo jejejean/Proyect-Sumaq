39587d4014dacc23da01d09ed2140779fdcabd24
jejejean <u19212532@utp.edu.pe>
2024-03-09 13:48:46 -0500

    feat :sparkles: implement footer accordion function

--- src/app/shared/components/layout/footer/footer.component.ts
+++ src/app/shared/components/layout/footer/footer.component.ts
@@ -5,28 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit {
-  accordionStates: { [key: string]: boolean } = {
-    nosotros: false,
-    atencionCliente: false,
-    libroDeReclamaciones: false,
-    sumaq: false,
-  };
+export class FooterComponent {
+  
+  openAccordion: string | null = null;
 
-  toggleAccordion(accordionName: string) {
-    const isOpen = this.accordionStates[accordionName];
-
-    // Cierra todos los acordeones
-    for (let key in this.accordionStates) {
-      this.accordionStates[key] = false;
-    }
-
-    // Si el acordeón clickeado ya estaba abierto, lo dejamos cerrado.
-    // Si no, lo abrimos.
-    this.accordionStates[accordionName] = !isOpen;
+  toggleAccordion(accordion: string) {
+    this.openAccordion = this.openAccordion === accordion ? null : accordion;
   }
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
--- src/app/shared/components/utils/accordion-item/accordion-item.component.ts
+++ src/app/shared/components/utils/accordion-item/accordion-item.component.ts
@@ -5,17 +5,11 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './accordion-item.component.html',
   styleUrls: ['./accordion-item.component.css'],
 })
-export class AccordionItemComponent implements OnInit {
+export class AccordionItemComponent {
   @Input() isOpen!: boolean;
   @Input() title!: string;
-  @Input() accordionState!: boolean;
   @Input() isList: boolean = true;
   @Input() itemsList!: string[];
   @Input() itemsText!: string;
-  toggleAccordion() {
-    this.isOpen = !this.isOpen;
-  }
-  constructor() {}
-
-  ngOnInit(): void {}
+  @Input() toggleAccordion!: (title: string) => void;
 }
